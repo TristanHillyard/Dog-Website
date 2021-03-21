54cdc286aa0fd8960ccff991af0f1c663a09251d
Tristan <tristan.hillyard@gmail.com>
2021-03-20 22:51:48 -0600

    Create modal for pictures

--- JavaScript/dogwebsite.js
+++ JavaScript/dogwebsite.js
@@ -53,3 +53,38 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 5000); //Changes every 5 seconds
+}
+
+var modal = document.getElementById('myModal');
+
+//Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName('mySlides');
+var modalImg = document.getElementsByClassName("modal_Images");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.caption;
+}
+
+//Get <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
